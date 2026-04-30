f57881f572a1557cbd8613ae8f9ac46cf3212436
Ons Elfekih <elfekihons@gmail.com>
2026-04-30 20:10:48 +0100

    ListeDesAgents

--- ReclamAppFrontend/src/app/pages/agents/agent-list/agent-list.ts
+++ ReclamAppFrontend/src/app/pages/agents/agent-list/agent-list.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AgentService } from '../../../core/services/agent.service';
+import { Agent } from '../../../core/models/agent.model';
 
 @Component({
   selector: 'app-agent-list',
-  imports: [],
-  templateUrl: './agent-list.html',
-  styleUrl: './agent-list.css',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './agent-list.html'
 })
-export class AgentList {}
+export class AgentList implements OnInit {
+
+  agents: Agent[] = [];
+
+  constructor(
+    private agentService: AgentService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    this.agentService.findAll().subscribe({
+      next: (data) => {
+        this.agents = data;
+        this.cdr.detectChanges();
+      },
+      error: (err) => console.error(err)
+    });
+  }
+}
